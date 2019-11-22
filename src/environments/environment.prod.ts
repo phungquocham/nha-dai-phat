f9b4944e43526f03bd2714175cec47718b15db30
Hung Pham <calich18@gmail.com>
2019-11-22 19:21:45 +0700

    ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   FIREBASE: {
-    apiKey: 'AIzaSyD2kTWAgvh13Kx7oOsocZDQe5VGXejZ4Q0',
+    apiKey: 'AIzaSyBgPW41JiJ5UJYQ0NM7SnxjJIBl_k0j8DQ',
     authDomain: 'https://nhadaiphat.net',
-    databaseURL: 'https://nha-dai-phat.firebaseapp.com',
-    projectId: 'nha-dai-phat',
-    storageBucket: 'nha-dai-phat.appspot.com'
+    databaseURL: 'https://nhadaiphat-prod.firebaseapp.com',
+    projectId: 'nhadaiphat-prod',
+    storageBucket: 'nhadaiphat-prod.appspot.com'
   },
   API_DOMAIN: 'https://api.nhadaiphat.net'
 };
