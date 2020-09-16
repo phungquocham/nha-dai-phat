4f68b1abe9c221cbba75faac5a9f8f6af5c8fd6c
Duy Tran <duyta@rever.vn>
2020-09-16 15:59:06 +0700

    Updating export excel reports

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,6 +13,7 @@ import { SnackbarService } from '../../shared/services/others/snackbar.service';
 import { WindowService } from 'src/app/shared/services/others/window.service';
 import { REGEX } from 'src/app/shared/helpers/regex';
 import { SNACKBAR } from 'src/app/shared/helpers/snackbar';
+import { ExportExcelService } from 'src/app/shared/services/api/export-excel.service';
 
 @Component({
   selector: 'app-login',
@@ -27,13 +28,68 @@ export class LoginComponent implements OnInit, AfterViewInit {
   showElementCodeForm = false;
   isError = false;
 
+  dataForExcel = [];
+  empPerformance = [
+    {
+      ID: 10011,
+      NAME: 'A',
+      DEPARTMENT: 'Sales',
+      MONTH: 'Jan',
+      YEAR: 2020,
+      SALES: 132412,
+      CHANGE: 12,
+      LEADS: 35,
+    },
+    {
+      ID: 10012,
+      NAME: 'A',
+      DEPARTMENT: 'Sales',
+      MONTH: 'Feb',
+      YEAR: 2020,
+      SALES: 232324,
+      CHANGE: 2,
+      LEADS: 443,
+    },
+    {
+      ID: 10013,
+      NAME: 'A',
+      DEPARTMENT: 'Sales',
+      MONTH: 'Mar',
+      YEAR: 2020,
+      SALES: 542234,
+      CHANGE: 45,
+      LEADS: 345,
+    },
+    {
+      ID: 10014,
+      NAME: 'A',
+      DEPARTMENT: 'Sales',
+      MONTH: 'Apr',
+      YEAR: 2020,
+      SALES: 223335,
+      CHANGE: 32,
+      LEADS: 234,
+    },
+    {
+      ID: 10015,
+      NAME: 'A',
+      DEPARTMENT: 'Sales',
+      MONTH: 'May',
+      YEAR: 2020,
+      SALES: 455535,
+      CHANGE: 21,
+      LEADS: 12,
+    },
+  ];
+
   constructor(
     public fb: FormBuilder,
     public router: Router,
     private authService: AuthenticationService,
     private windowService: WindowService,
     private snackbar: SnackbarService,
-    private auth: AuthenticationService
+    private auth: AuthenticationService,
+    public exportExcel: ExportExcelService
   ) {
     this.form = this.fb.group({
       phone: [
@@ -75,8 +131,8 @@ export class LoginComponent implements OnInit, AfterViewInit {
   }
 
   onSubmit(): void {
-    if (this.form.controls['phone'].valid) {
-      const phone = this.form.value['phone'];
+    if (this.form.controls.phone.valid) {
+      const phone = this.form.value.phone;
       this.showElementCodeForm = true;
       this.authService
         .signInWithPhoneNumber({
@@ -84,7 +140,11 @@ export class LoginComponent implements OnInit, AfterViewInit {
           applicationVerifier: this.windowRef.recaptchaVerifier,
         })
         .then((confirmationResult) => {
-          console.log('SUBMIT LOGIN', this.replaceAt(phone, 0, '+84'), this.windowRef.recaptchaVerifier);
+          console.log(
+            'SUBMIT LOGIN',
+            this.replaceAt(phone, 0, '+84'),
+            this.windowRef.recaptchaVerifier
+          );
           console.log('SUBMIT RESULT', confirmationResult);
           // SMS sent. Prompt user to type the code from the message, then sign the
           // user in with confirmationResult.confirm(code).
@@ -101,7 +161,7 @@ export class LoginComponent implements OnInit, AfterViewInit {
   }
 
   verifyLoginCode() {
-    this.verificationCode = this.form.value['verifyCode'];
+    this.verificationCode = this.form.value.verifyCode;
     console.log('AAAAAA', this.windowRef, this.windowRef.confirmationResult);
     this.windowRef.confirmationResult
       .confirm(this.verificationCode)
@@ -113,5 +173,21 @@ export class LoginComponent implements OnInit, AfterViewInit {
         this.snackbarError('Sai mã đăng nhập. Vui lòng thử lại.');
         console.log(error);
       });
+  }
+
+  exportToExcel() {
+    this.empPerformance.forEach((row: any) => {
+      this.dataForExcel.push(Object.values(row));
+    });
+
+    const reportData = {
+      title: 'Employee Sales Report - Jan 2020',
+      data: this.dataForExcel,
+      headers: Object.keys(this.empPerformance[0]),
+    };
+
+    console.log(reportData);
+
+    this.exportExcel.exportExcel(reportData);
   }
 }
---
+++ src/app/shared/services/api/export-excel.service.ts
@@ -0,0 +1,345 @@
+import { Injectable } from '@angular/core';
+import { Workbook, Worksheet } from 'exceljs';
+import * as fs from 'file-saver';
+
+interface HeaderCell {
+  worksheet: Worksheet;
+  value: any;
+
+  mergeCells?: string;
+  font?: object;
+  alignment?: object;
+  border?: object;
+  width?: number;
+  height?: number;
+  cellName?: string;
+}
+
+interface ChildHeaderCell {
+  cellName: string;
+  values: any[];
+  xColumns: any[];
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExportExcelService {
+  private xColumns = [];
+  private alphaStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  constructor() {
+    this.xColumns = this.alphaStr.split('');
+  }
+
+  private createHeaderCell(headerCell: HeaderCell) {
+    let cellName = '';
+    if (headerCell.mergeCells) {
+      const cells = headerCell.mergeCells.split(':');
+      if (cells.length > 1) {
+        headerCell.worksheet.mergeCells(headerCell.mergeCells);
+      }
+      cellName = cells[0];
+    } else if (headerCell.cellName) {
+      cellName = headerCell.cellName;
+    }
+
+    const cell = headerCell.worksheet.getCell(cellName);
+    cell.value = headerCell.value;
+
+    cell.font = {
+      name: 'Calibri',
+      size: 12,
+      bold: true,
+    };
+
+    if (headerCell.font) {
+      cell.font = { ...cell.font, ...headerCell.font };
+    }
+
+    cell.alignment = { vertical: 'middle', horizontal: 'center' };
+
+    cell.border = {
+      bottom: { style: 'thin' },
+      right: { style: 'thin' },
+    };
+
+    if (headerCell.width) {
+      const key = cellName.split('')[0];
+      headerCell.worksheet.getColumn(key).width = headerCell.width;
+    }
+  }
+
+  private getChildHeaderCells(childHeaderCell: ChildHeaderCell) {
+    const [xCellName, yCellName] = childHeaderCell.cellName.split('');
+
+    const index = childHeaderCell.xColumns.indexOf(xCellName);
+
+    if (index > -1) {
+      return childHeaderCell.xColumns
+        .slice(index, index + childHeaderCell.values.length)
+        .map((character) => character + yCellName);
+    }
+    return [];
+  }
+
+  exportExcel(excelData) {
+    // Title, Header & Data
+    const title = excelData.title;
+    const header = excelData.headers;
+    const data = excelData.data;
+
+    // Create a workbook with a worksheet
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet('Sales Data');
+
+    // Freeze Header & first column
+    worksheet.views = [
+      {
+        state: 'frozen',
+        xSplit: 1,
+        ySplit: 2,
+        activeCell: 'A1',
+      },
+    ];
+
+    this.createHeaderCell({
+      worksheet,
+      mergeCells: 'A1:A2',
+      value: 'TÊN',
+    });
+
+    this.createHeaderCell({
+      worksheet,
+      mergeCells: 'B1:B2',
+      value: 'GIỜ VÀNG',
+      width: 20,
+    });
+
+    this.createHeaderCell({
+      worksheet,
+      mergeCells: 'C1:I1',
+      value: 'ĐỔ ĐỀU ĐẶN',
+    });
+    const columns = ['KLL', 'KBM', 'KNC', 'CM', '2 câu', '> 2 câu', 'Tổng'];
+    const cells = this.getChildHeaderCells({
+      cellName: 'C2',
+      values: columns,
+      xColumns: this.xColumns,
+    });
+    cells.forEach((cell, index) => {
+      this.createHeaderCell({
+        worksheet,
+        cellName: cell,
+        value: columns[index],
+        font: {
+          bold: false,
+        },
+        width: 10,
+      });
+    });
+
+    // Add Row and formatting
+    // worksheet.mergeCells('C1:F4');
+    // const titleRow = worksheet.getCell('C1');
+    // titleRow.value = title;
+    // titleRow.font = {
+    //   name: 'Calibri',
+    //   size: 16,
+    //   underline: 'single',
+    //   bold: true,
+    //   color: { argb: '0085A3' },
+    // };
+    // titleRow.alignment = { vertical: 'middle', horizontal: 'center' };
+
+    // Date
+    // worksheet.mergeCells('G1:H4');
+    // const d = new Date();
+    // const date = d.getDate() + '-' + d.getMonth() + '-' + d.getFullYear();
+    // const dateCell = worksheet.getCell('G1');
+    // dateCell.value = date;
+    // dateCell.font = {
+    //   name: 'Calibri',
+    //   size: 12,
+    //   bold: true,
+    // };
+    // dateCell.alignment = { vertical: 'middle', horizontal: 'center' };
+
+    // Add Image
+    // const myLogoImage = workbook.addImage({
+    //   base64: logo.imgBase64,
+    //   extension: 'png',
+    // });
+    // worksheet.mergeCells('A1:B4');
+    // worksheet.addImage(myLogoImage, 'A1:B4');
+
+    // Blank Row
+    // worksheet.addRow([]);
+
+    // Adding Header Row
+    // const headerRow = worksheet.addRow(header);
+    // // headerRow.eachCell((cell, number) => {
+    // headerRow.eachCell((cell) => {
+    //   cell.fill = {
+    //     type: 'pattern',
+    //     pattern: 'solid',
+    //     fgColor: { argb: '4167B8' },
+    //     bgColor: { argb: '' },
+    //   };
+    //   cell.font = {
+    //     bold: false,
+    //     color: { argb: 'FFFFFF' },
+    //     size: 12,
+    //   };
+    // });
+
+    // Adding Data with Conditional Formatting
+    data.forEach((item) => {
+      const row = worksheet.addRow(item);
+
+      const sales = row.getCell(6);
+      let color = 'FF99FF99';
+      if (+sales.value < 200000) {
+        color = 'FF9999';
+      }
+
+      sales.fill = {
+        type: 'pattern',
+        pattern: 'solid',
+        fgColor: { argb: color },
+      };
+    });
+
+    // worksheet.getColumn(3).width = 20;
+    // worksheet.addRow([]);
+
+    // Footer Row
+    // const footerRow = worksheet.addRow([
+    //   'Employee Sales Report Generated from example.com at ' + date,
+    // ]);
+    // footerRow.getCell(1).fill = {
+    //   type: 'pattern',
+    //   pattern: 'solid',
+    //   fgColor: { argb: 'FFB050' },
+    // };
+
+    // Merge Cells
+    // worksheet.mergeCells(`A${footerRow.number}:F${footerRow.number}`);
+
+    // Generate & Save Excel File
+    workbook.xlsx.writeBuffer().then((resp) => {
+      const blob = new Blob([resp], {
+        type:
+          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      });
+      fs.saveAs(blob, title + '.xlsx');
+    });
+  }
+}
+
+// exportExcel(excelData) {
+//   // Title, Header & Data
+//   const title = excelData.title;
+//   const header = excelData.headers;
+//   const data = excelData.data;
+
+//   // Create a workbook with a worksheet
+//   const workbook = new Workbook();
+//   const worksheet = workbook.addWorksheet('Sales Data');
+
+//   // Add Row and formatting
+//   // worksheet.mergeCells('C1:F4');
+//   // const titleRow = worksheet.getCell('C1');
+//   // titleRow.value = title;
+//   // titleRow.font = {
+//   //   name: 'Calibri',
+//   //   size: 16,
+//   //   underline: 'single',
+//   //   bold: true,
+//   //   color: { argb: '0085A3' },
+//   // };
+//   // titleRow.alignment = { vertical: 'middle', horizontal: 'center' };
+
+//   // Date
+//   // worksheet.mergeCells('G1:H4');
+//   // const d = new Date();
+//   // const date = d.getDate() + '-' + d.getMonth() + '-' + d.getFullYear();
+//   // const dateCell = worksheet.getCell('G1');
+//   // dateCell.value = date;
+//   // dateCell.font = {
+//   //   name: 'Calibri',
+//   //   size: 12,
+//   //   bold: true,
+//   // };
+//   // dateCell.alignment = { vertical: 'middle', horizontal: 'center' };
+
+//   // Add Image
+//   // const myLogoImage = workbook.addImage({
+//   //   base64: logo.imgBase64,
+//   //   extension: 'png',
+//   // });
+//   // worksheet.mergeCells('A1:B4');
+//   // worksheet.addImage(myLogoImage, 'A1:B4');
+
+//   // Blank Row
+//   // worksheet.addRow([]);
+
+//   // Adding Header Row
+//   const headerRow = worksheet.addRow(header);
+//   // headerRow.eachCell((cell, number) => {
+//   headerRow.eachCell((cell) => {
+//     cell.fill = {
+//       type: 'pattern',
+//       pattern: 'solid',
+//       fgColor: { argb: '4167B8' },
+//       bgColor: { argb: '' },
+//     };
+//     cell.font = {
+//       bold: false,
+//       color: { argb: 'FFFFFF' },
+//       size: 12,
+//     };
+//   });
+
+//   // Adding Data with Conditional Formatting
+//   data.forEach((item) => {
+//     const row = worksheet.addRow(item);
+
+//     const sales = row.getCell(6);
+//     let color = 'FF99FF99';
+//     if (+sales.value < 200000) {
+//       color = 'FF9999';
+//     }
+
+//     sales.fill = {
+//       type: 'pattern',
+//       pattern: 'solid',
+//       fgColor: { argb: color },
+//     };
+//   });
+
+//   worksheet.getColumn(3).width = 20;
+//   worksheet.addRow([]);
+
+//   // Footer Row
+//   // const footerRow = worksheet.addRow([
+//   //   'Employee Sales Report Generated from example.com at ' + date,
+//   // ]);
+//   // footerRow.getCell(1).fill = {
+//   //   type: 'pattern',
+//   //   pattern: 'solid',
+//   //   fgColor: { argb: 'FFB050' },
+//   // };
+
+//   // Merge Cells
+//   // worksheet.mergeCells(`A${footerRow.number}:F${footerRow.number}`);
+
+//   // Generate & Save Excel File
+//   workbook.xlsx.writeBuffer().then((resp) => {
+//     const blob = new Blob([resp], {
+//       type:
+//         'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+//     });
+//     fs.saveAs(blob, title + '.xlsx');
+//   });
+// }
